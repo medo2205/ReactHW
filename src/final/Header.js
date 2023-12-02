8baafe6ef6ce2f824494590e2b5ee440426b018a
Mohammed Alamri <tu4102505@taibahu.edu.sa>
2023-12-02 23:51:26 +0300

    3

--- src/final/Header.js
+++ src/final/Header.js
@@ -4,7 +4,7 @@ class Header extends Component{
     render(){
         return(
             <div className="navbar bg-dark rounded text-white">
-                <h2>Our Team</h2>
+                <h2>My Team</h2>
             </div>
         )
     }
